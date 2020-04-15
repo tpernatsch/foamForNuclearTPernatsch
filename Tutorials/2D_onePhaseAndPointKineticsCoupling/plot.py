42594533303a0bd3132f15e3a5e0d12a69fb808a
virmodoetiae <stefan.radman@epfl.ch>
2020-04-15 17:40:56 +0200

    Updated tutorials

--- Tutorials/2D_onePhaseAndPointKineticsCoupling/plot.py
+++ Tutorials/2D_onePhaseAndPointKineticsCoupling/plot.py
@@ -1,30 +1,58 @@
-from matplotlib import pyplot as plt
-import os
+### IMPORTS
 
-startTime = 200
-fig, (ax1, ax2) = plt.subplots(2,1)
+import sys
+import matplotlib.pyplot as plt
 
-with open("GeN-Foam.dat") as f:
-	time = []
-	power = []
-	avFuelTemp = []
-	lines = f.readlines()
-	for line in lines :
-		splitLine = line.split(";")
-		try :
-			time.append(float(splitLine[0])-startTime)
-			power.append(float(splitLine[2]))
-			avFuelTemp.append(float(splitLine[6]))
-		except :
-			pass
+### MAIN
 
-	ax1.semilogx(time, power)
-	ax1.set_xlabel("Time (s)")
-	ax1.set_ylabel("Power (W)")
-	ax1.grid(True)
-	ax2.semilogx(time, avFuelTemp)
-	ax2.set_xlabel("Time (s)")
-	ax2.set_ylabel("Average Fuel Temperature (K)")
-	ax2.grid(True)
-	plt.subplots_adjust(hspace=0.5)
-	plt.show()
+fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+
+ax1.set_ylabel("power (W)")
+ax2.set_ylabel("reactivity (pcm)")
+ax3.set_ylabel("fuelTemperature (K)")
+ax3.set_xlabel("time(s)")
+
+for q in range(len(sys.argv)-1) :
+
+    filename = sys.argv[q+1]
+    file = open(filename, "r")
+    lines = file.readlines()
+    powers = []
+    times = []
+    totRhos = []
+    TFuels = []
+    i = 0
+    power = 0
+    time = 0
+    totRho = 0
+    TFuel = 0
+    for line in lines :
+        if "power = " in line :
+            power = float(line.split()[2])
+        if "totalReactivity" in line :
+            totRho = float(line.split()[2])
+        if "TFuel = " in line :
+            TFuel = float(line.split()[2])
+        if "Time =" in line and not "ExecutionTime" in line :
+            time = float(line.split()[2])
+            powers.append(power)
+            times.append(time)
+            totRhos.append(totRho)
+            TFuels.append(TFuel)
+    del totRhos[0]
+    del powers[0]
+    del TFuels[0]
+    t0 = times[0]
+    for i in range(len(times)) :
+    	times[i] = times[i] - t0  
+    del times[-1]
+    ax1.semilogx(times, powers, label=filename)
+    ax2.semilogx(times, totRhos, label=filename)  
+    ax3.semilogx(times, TFuels, label=filename)      
+ax1.legend()
+ax1.grid(True)
+ax2.legend()
+ax2.grid(True)
+ax3.legend()
+ax3.grid(True)
+plt.show()
