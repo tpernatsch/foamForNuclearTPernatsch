9736cd2ad255fc70afec56e125fcc2ebf9bcc9b6
gauthier.lazaregit config --global user.email gauthier.lazare@epfl.ch <gauthier.lazare@epfl.ch>
2021-12-16 13:17:15 +0100

    Added preliminary model of CHF and post-CHF

---
+++ GeN-Foam/classes/thermalHydraulics/src/physicsModels/heatTransferModels/FSHeatTransferCoefficientModels/CHFModels/LeidenFrostTemperatureModels/TLFModel.C
@@ -0,0 +1,71 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "FSPair.H"
+#include "TLFModel.H"
+#include "myOps.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(TLFModel, 0);
+    defineRunTimeSelectionTable
+    (
+        TLFModel, 
+        LeidenFrostTemperatureModels
+    );
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::TLFModel::TLFModel
+(
+    const FSPair& pair,
+    const dictionary& dict,
+    const objectRegistry& objReg
+)
+:
+    IOdictionary
+    (
+        IOobject
+        (
+            "TLFModel."+typeName,
+            pair.mesh().time().timeName(),
+            objReg,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        dict
+    ),
+    pair_(pair),
+    Tsat_(pair.mesh().lookupObject<volScalarField>("T.interface")),
+    p_(pair.mesh().lookupObject<volScalarField>("p"))
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+
+// ************************************************************************* //
