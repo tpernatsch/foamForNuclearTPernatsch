465c345355678c789327e5c8bb6809a49f7b8b3c
carlo <carlo.fiorina@outlook.com>
2022-07-14 13:22:43 +0200

    Added new heat transfer model to allow for Nusselt plus a heat resistence. Useful for HXs

---
+++ GeN-Foam/classes/thermalHydraulics/src/physicsModels/heatTransferModels/FSHeatTransferCoefficientModels/NusseltAndWall/NusseltAndWallFSHeatTransferCoefficient.C
@@ -0,0 +1,107 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "FSPair.H"
+#include "NusseltAndWallFSHeatTransferCoefficient.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace FSHeatTransferCoefficientModels
+{
+    defineTypeNameAndDebug(NusseltAndWall, 0);
+    addToRunTimeSelectionTable
+    (
+        FSHeatTransferCoefficientModel,
+        NusseltAndWall,
+        FSHeatTransferCoefficientModels
+    );
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::FSHeatTransferCoefficientModels::NusseltAndWall::NusseltAndWall
+(
+    const FSPair& pair,
+    const dictionary& dict,
+    const objectRegistry& objReg
+)
+:
+    FSHeatTransferCoefficientModel
+    (
+        pair,
+        dict,
+        objReg
+    ),
+    Re_(pair.Re()),
+    kappa_(pair.fluidRef().kappa()),
+    Pr_(pair.fluidRef().Pr()),
+    Dh_(pair.fluidRef().Dh()),
+    A_(dict.get<scalar>("const")),
+    B_(dict.get<scalar>("coeff")),
+    C_(dict.get<scalar>("expRe")),
+    D_(dict.get<scalar>("expPr")),
+    H_wall(dict.get<scalar>("addH")),
+    usePeclet_(C_ == D_)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::scalar Foam::FSHeatTransferCoefficientModels::NusseltAndWall::value
+(
+    const label& celli
+) const
+{
+    //- I am creating a scalar on return to (hopefully) force Return Value
+    //  Optimizations (RVOs, C++ performance stuff)
+    scalar H_fluid;
+    if (B_ != 0)
+    {
+        if (usePeclet_)
+            H_fluid = scalar
+            (
+                (kappa_[celli]/Dh_[celli])*
+                (A_ + B_*pow(Re_[celli]*Pr_[celli], C_))
+            );
+        else
+            H_fluid = scalar
+            (
+                (kappa_[celli]/Dh_[celli])*
+                (A_ + B_*pow(Re_[celli], C_)*pow(Pr_[celli], D_))
+            );
+    }
+    else
+        H_fluid = scalar((kappa_[celli]/Dh_[celli])*A_);
+
+    return(H_fluid*H_wall / (H_fluid + H_wall));
+}
+
+
+// ************************************************************************* //
