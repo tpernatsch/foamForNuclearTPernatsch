9346ba45fdeebb918872f70da98aeea321b2105f
Thomas Guilbaud <thomas.guilbaud@epfl.ch>
2023-06-18 12:27:33 -0500

    tuto: Update header to GeN-Foam banner

--- Tutorials/2D_onePhaseAndSubcriticalPointKineticsCoupling/rootCase/blockMesh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cp system/blockMeshDict system/fluidRegion
-cp system/blockMeshDict system/neutroRegion
-cp system/blockMeshDict system/thermoMechanicalRegion
-
-blockMesh -region fluidRegion
-blockMesh -region neutroRegion
-blockMesh -region thermoMechanicalRegion
