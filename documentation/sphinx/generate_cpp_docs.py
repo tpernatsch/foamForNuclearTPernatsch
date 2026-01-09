d17010bc73e7edff8ac2a9dec75d16c07fda63ae
foam-for-nuclear project <carlo.fiorina@outlook.com>
2026-01-09 15:44:28 +0000

    Update file generate_cpp_docs.py

--- documentation/sphinx/generate_cpp_docs.py
+++ documentation/sphinx/generate_cpp_docs.py
@@ -887,7 +887,6 @@ def main():
     )
 
     # Step 3: Append indexes in dynamic files
-    """
     append_toctree_for_folder_recursive(
         target_rst="documentation/sphinx/usersguide/modules/thermalHydraulics/porousMedium/FFdrag.rst",
         folder="documentation/sphinx/cppapi/generated/porousMediaModels/physicsModels/dragModels/FFDragCoefficientModels",
@@ -898,7 +897,6 @@ def main():
         folder="documentation/sphinx/cppapi/generated/modules/thermalHydraulics/",
         maxdepth=2
     )
-    """
 
 
     print("End")
