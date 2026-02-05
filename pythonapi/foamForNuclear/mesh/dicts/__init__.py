3b4dfc66d3121e67a60f45876df42890a378c279
AlessandroScolaro <AlessandroScolaro@gitlab.com>
2026-02-05 14:20:02 +0100

    api: moved mesh dicts to mesh/dicts

---
+++ pythonapi/foamForNuclear/mesh/dicts/__init__.py
@@ -0,0 +1,12 @@
+from ._createBafflesDict import (BaffleDict, CreateBafflesDict)
+from ._createPatchDict import (PatchInfo, CreatePatchDict)
+from ._decomposeParDict import (DecomposeParDict)
+from ._dynamicMeshDict import (
+    MotionDict, LinearMotion, OscillatingLinearMotion, RotatingMotion, OscillatingRotatingMotion, DiffusivityMotion, UniformDiffusivityMotion, DirectionalDiffusivityMotion, MotionDirectionalDiffusivityMotion, QuadraticDiffusivityMotion, FileDiffusivityMotion, DynamicMeshDict,
+)
+from ._topoSetDict import (TopoSetAction, TopoSetDict)
+
+__all__ = (
+    "BaffleDict", "CreateBafflesDict", "PatchInfo", "CreatePatchDict", "DecomposeParDict"
+    "MotionDict", "LinearMotion", "OscillatingLinearMotion", "RotatingMotion", "OscillatingRotatingMotion", "DiffusivityMotion", "UniformDiffusivityMotion", "DirectionalDiffusivityMotion", "MotionDirectionalDiffusivityMotion", "QuadraticDiffusivityMotion", "FileDiffusivityMotion", "DynamicMeshDict", "TopoSetAction", "TopoSetDict",
+    )
