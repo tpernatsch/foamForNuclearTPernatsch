cf33c680fb4f0983710f6cf061a78a10c03cfa18
AlessandroScolaro <AlessandroScolaro@gitlab.com>
2026-02-07 13:17:40 +0100

    api: accepted OpenFOAMFile as attrs class

--- pythonapi/foamForNuclear/openfoamFile.py
+++ pythonapi/foamForNuclear/openfoamFile.py
@@ -6,7 +6,18 @@ import foamlib
 from foamForNuclear.common import *
 from foamForNuclear.checkvalue import check_type
 
+import attrs as attr
+from attrs import define, field
+import attrs.validators as v
+from ._attrs_tools import auto_type_validator
 
+
+@define(
+    slots=True,
+    on_setattr=[attr.setters.convert, attr.setters.validate],
+    field_transformer=auto_type_validator,
+    repr=False,
+)
 class OpenFOAMFile:
     """
     Base class for OpenFOAM file
@@ -22,56 +33,10 @@ class OpenFOAMFile:
     ext : str
         Extension at the end of the file name, e.g `".liquid"` (default: `''`)
     """
-    def __init__(self, name, folder: str="", region: str="", ext: str=""):
-        self.name = name
-        self.folder = folder
-        self.region = region
-        self.ext = ext
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @name.setter
-    def name(self, name: str) -> None:
-        check_type("name", name, str, none_ok=True)
-        self._name = name
-
-    @property
-    def folder(self) -> str:
-        return self._folder
-
-    @folder.setter
-    def folder(self, folder: str) -> None:
-        if folder is not None:
-            check_type("folder", folder, str)
-            self._folder = folder
-        else:
-            self._folder = ''
-
-    @property
-    def region(self) -> str:
-        return self._region
-
-    @region.setter
-    def region(self, region: str) -> None:
-        if region is not None:
-            check_type("region", region, str)
-            self._region = region
-        else:
-            self._region = ''
-
-    @property
-    def ext(self) -> str:
-        return self._ext
-
-    @ext.setter
-    def ext(self, ext: str) -> None:
-        if ext is not None:
-            check_type("ext", ext, str)
-            self._ext = ext
-        else:
-            self._ext = ''
+    name: str = ""
+    folder: str = ""
+    region: str = ""
+    ext: str = ""
 
     @property
     def path(self):
@@ -99,7 +64,6 @@ class OpenFOAMFile:
 
             text  = openfoamHeader
             text += openfoamFileHeader(self.name)
-
             text += func(self, *args, **kwargs)
 
             text += openfoamFooterLine
